f091bec4c431d462e5243e3346dd97a3ab72e343
cstransky <csstransky@gmail.com>
2021-03-19 12:28:14 -0400

    Chapter 2 Lesson 4

--- Exercise Files/Ch02/02_04/start/react-essentials/src/index.js
+++ Exercise Files/Ch02/02_04/start/react-essentials/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 ReactDOM.render(
-  React.createElement(
-    "h1",
-    { style: { color: "blue" } },
-    "Heyyy Everyone!"
-  ),
+  <ul>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+  </ul>,
   document.getElementById("root")
 );

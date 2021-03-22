91872a3137bd4119bb80d1d05a97ab934baeb8f5
cstransky <csstransky@gmail.com>
2021-03-22 13:02:33 -0400

    Chapter 7 Lesson 2

--- Exercise Files/Ch07/07_02/start/react-essentials/src/pages.js
+++ Exercise Files/Ch07/07_02/start/react-essentials/src/pages.js
@@ -1,50 +1,40 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom"
+import React from "react"
 
 export function Home() {
-  return (
-    <div>
-      <h1>[Company Website]</h1>
-        <nav>
-            <li><Link to={"about"}>About</Link></li>
-            <li><Link to={"events"}>Events</Link></li>
-            <li><Link to={"contact"}>Contact</Link></li>
-        </nav>
-    </div>
-  );
-}
-
-export function About() {
-  return (
-    <div>
-      <h1>[About]</h1>
-    </div>
-  );
-}
-
-export function Events() {
-  return (
-    <div>
-      <h1>[Events]</h1>
-    </div>
-  );
-}
-
-export function Contact() {
-  return (
-    <div>
-      <h1>[Contact]</h1>
-    </div>
-  );
-}
-
-export function Whoops404() {
-    let location = useLocation();
-    console.log(location)
     return (
         <div>
             <h1>
-                This page "{location.pathname}" does not exist!
+                [Company Website]
+            </h1>
+        </div>
+    )
+}
+
+export function About() {
+    return (
+        <div>
+            <h1>
+                [About]
+            </h1>
+        </div>
+    )
+}
+
+export function Events() {
+    return (
+        <div>
+            <h1>
+                [Events]
+            </h1>
+        </div>
+    )
+}
+
+export function Contact() {
+    return (
+        <div>
+            <h1>
+                [Contact]
             </h1>
         </div>
     )

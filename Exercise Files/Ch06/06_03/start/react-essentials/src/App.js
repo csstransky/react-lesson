4066f1d900fae8c14f68a24834328c15f10a8b0b
cstransky <csstransky@gmail.com>
2021-03-22 11:40:41 -0400

    Chapter 6 Lesson 3

--- Exercise Files/Ch06/06_03/start/react-essentials/src/App.js
+++ Exercise Files/Ch06/06_03/start/react-essentials/src/App.js
@@ -1,40 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-// https://api.github.com/users/eveporcello
-
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if (loading) return <h1>Loading...</h1>;
-
-  if (error) {
-    return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  }
-
-  if (data) {
-    return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
-      </div>
-    );
-  }
-
-  return <div>No Data Available</div>;
+function App() {
+  return (
+    <div>
+      <h1>Hello React Testing Library lol</h1>
+    </div>
+  );
 }
 
 export default App;
